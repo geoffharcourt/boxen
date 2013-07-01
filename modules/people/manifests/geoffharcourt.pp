f362d4d5c997a49695fd3c454b629ee11d777994
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-07-01 17:52:40 -0400

    Add personal software

--- modules/people/manifests/geoffharcourt.pp
+++ modules/people/manifests/geoffharcourt.pp
@@ -1,4 +1,32 @@
 class people::geoffharcourt {
+  include adium
+  include caffeine
+  include chrome
+  include ctags
+  include cyberduck
+  include dropbox
+  include elasticsearch
+  include flux
+  include foreman
+  include ghostscript
+  include github_for_mac
+  include googledrive
+  include heroku
+  include hub
+  include imagemagick
+  include onepassword
+  include pgadmin3
+  include postgresql
+  include pow
+  include qt
+  include redis
+  include skype
+  include sparrow
+  include spotify
+  include things
+  include tmux
+  include wget
+
 
 }
 
